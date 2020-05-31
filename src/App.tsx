aef8d8d6d7af4b518692929bcffd5163e9416bac
Radionic <khpluslt@gmail.com>
2020-05-31 14:29:37 +0800

    [Android] Add back button support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 import Page from './pages/page'
 import MainActivity from './pages/MainActivity'
 import Settings from './pages/Settings'
 
 import { useRecoilState } from 'recoil'
 
-import { pageState } from './states/app-state'
+import { pageState, drawerVisibleState } from './states/app-state'
+import { BackgroundMode } from '@ionic-native/background-mode'
 
 import './App.css'
 
@@ -13,7 +15,29 @@ import './App.css'
 
 const App: React.FC = props => {
   const [page, setPage] = useRecoilState(pageState)
+  const [drawerVisible, setDrawerVisible] = useRecoilState(drawerVisibleState)
 
+  useEffect(() => {
+    const backButtonListener = () => {
+      if (drawerVisible) {
+        setDrawerVisible(false)
+        return
+      }
+
+      switch (page) {
+        case Page.MainActivity:
+          BackgroundMode.moveToBackground()
+          break
+        case Page.Settings:
+          setPage(Page.MainActivity)
+          break
+      }
+    }
+
+    document.addEventListener('backbutton', backButtonListener)
+    return () => document.removeEventListener('backbutton', backButtonListener)
+  }, [drawerVisible, page])
+  
   const renderPage = () => {
     switch (page) {
       case Page.MainActivity:
